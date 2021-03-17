f8ab95ae8017b8bf91e390d2f408d13e95a06ea6
xb.wang <futaalice@emails.bjut.edu.cn>
2021-03-17 15:01:28 +0800

    first commit

---
+++ Source/LLTransServerProject/LLTransServerProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LLTransServerProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LLTRANSSERVERPROJECT_API ALLTransServerProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
