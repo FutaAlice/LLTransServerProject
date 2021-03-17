7b975b9f84401de488e81e2dc2ca0e5c1188e9b0
xb.wang <futaalice@emails.bjut.edu.cn>
2021-03-17 15:49:07 +0800

    Set timeout to zero

--- Source/LLTransServerProject/fuckwindows.cpp
+++ Source/LLTransServerProject/fuckwindows.cpp
@@ -12,7 +12,7 @@ std::string foo(std::map<std::string, float> kv)
 	json11::Json jo(kv);
 	std::string temp = jo.dump();
 
-	httplib::Client cli("localhost", 22222, 300);
+	httplib::Client cli("localhost", 22222, 0);
 
 	cli.Post("/bar", temp, "application/json");
     return temp;
